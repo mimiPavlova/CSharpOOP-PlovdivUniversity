49882b89df42afa1e7fb03416c95a1837cf6ede3
Мария Павлова <ivana.prego@gmail.com>
2025-06-18 17:53:09 +0300

    😶

---
+++ LybrarySystem/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LybrarySystem;
+
+public class Book
+{
+    public string Title { get; }
+    public string Author { get; }
+
+    public Book(string  title, string author)
+    {
+        if(string.IsNullOrWhiteSpace(title))throw new ArgumentNullException("title cannot be null");
+        this.Title = title;
+        if (string.IsNullOrWhiteSpace(author)) throw new ArgumentNullException("authour cannot be null");
+        this.Author = author;
+    }
+}
---
+++ LybrarySystem/Library.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LybrarySystem;
+
+public class Library
+{
+    public List<Book> Books;
+    public Library()
+    {
+        Books = new List<Book>();
+    }
+    public void AddBook(Book book)
+    {
+        if (Books.Any(b=>b.Title==book.Title&&b.Author==book.Author))
+        {
+            Console.WriteLine("Book alredy exists");
+        }
+        else
+        {
+            Books.Add(book);
+            Console.WriteLine("Book added");
+        }
+    }
+    public void RemoveBook(string title, string authour)
+    {
+        Book book = Books.FirstOrDefault(b => b.Title==title&&b.Author==authour);
+        if (book!=null)
+        {
+            Books.Remove(book);
+        }
+        else
+        {
+            Console.WriteLine("book doesn't exists in the library");
+        }
+    }
+
+    public void DisplayBooks()
+    {
+        if (Books.Count==0) Console.WriteLine("Library is empty");
+        foreach (Book book in Books)
+        {
+            Console.WriteLine($"{book.Title} --- {book.Author}");
+        }
+    }
+
+}
